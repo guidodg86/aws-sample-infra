d97fb1d4ffa3a61e3c5dc795d4f21b35e63a92ca
Guido de Giovanni <guidito.dg86@gmail.com>
2023-08-24 17:10:44 +0100

    Testing with one server

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Deploy testing vpc
 resource "aws_vpc" "testing_vpc" {
   cidr_block = var.vpc_cidr
   tags = {
@@ -5,13 +6,26 @@ resource "aws_vpc" "testing_vpc" {
   }
 }
 
-#Deploy the private subnets
+# Deploy the private subnets
 resource "aws_subnet" "private_subnets" {
-  for_each   = var.subnets
-  vpc_id     = aws_vpc.testing_vpc.id
-  cidr_block = each.value["cidr"]
+  for_each          = var.subnets
+  vpc_id            = aws_vpc.testing_vpc.id
+  cidr_block        = each.value["cidr"]
+  availability_zone = each.value["az"]
 
   tags = {
     Name = each.value["name"]
+  }
+}
+
+resource "aws_instance" "ubuntu_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private_subnets["server"].id
+  #security_groups             = [aws_security_group.vpc-ping.id, aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "printer-server"
   }
 }
