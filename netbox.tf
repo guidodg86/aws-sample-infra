e8938c7be1ec84e4b30a3f147325304980cd9bd1
Guido de Giovanni <guidito.dg86@gmail.com>
2023-08-31 17:42:04 +0100

    added subnets to netbox

--- netbox.tf
+++ netbox.tf
@@ -74,7 +74,103 @@ resource "netbox_ipam_role" "sec" {
 resource "netbox_prefix" "printer" {
   prefix      = "10.0.1.0/24"
   status      = "active"
-  site_id = netbox_site.aws.id
-  role_id = netbox_ipam_role.printer.id
+  site_id     = netbox_site.aws.id
+  role_id     = netbox_ipam_role.printer.id
   description = "R-PRINTER_NETGROUP-aws"
+}
+
+resource "netbox_prefix" "cctv" {
+  prefix      = "10.0.2.0/24"
+  status      = "active"
+  site_id     = netbox_site.aws.id
+  role_id     = netbox_ipam_role.cctv.id
+  description = "R-CCTV_NETGROUP-aws"
+}
+
+resource "netbox_prefix" "srv" {
+  prefix      = "10.0.3.0/24"
+  status      = "active"
+  site_id     = netbox_site.aws.id
+  role_id     = netbox_ipam_role.srv.id
+  description = "R-SRV_NETGROUP-aws"
+}
+
+resource "netbox_prefix" "database" {
+  prefix      = "10.0.4.0/24"
+  status      = "active"
+  site_id     = netbox_site.aws.id
+  role_id     = netbox_ipam_role.database.id
+  description = "R-DATABASE_NETGROUP-aws"
+}
+
+resource "netbox_prefix" "kubernetes" {
+  prefix      = "10.0.5.0/24"
+  status      = "active"
+  site_id     = netbox_site.aws.id
+  role_id     = netbox_ipam_role.kubernetes.id
+  description = "R-KUBERNETES_NETGROUP-aws"
+}
+
+resource "netbox_prefix" "hr-bue1" {
+  prefix      = "10.1.10.0/24"
+  status      = "active"
+  site_id     = netbox_site.bue1.id
+  role_id     = netbox_ipam_role.hr.id
+  description = "R-HR_NETGROUP-bue1"
+}
+
+resource "netbox_prefix" "hr-ltv2" {
+  prefix      = "10.2.10.0/24"
+  status      = "active"
+  site_id     = netbox_site.ltv2.id
+  role_id     = netbox_ipam_role.hr.id
+  description = "R-HR_NETGROUP-ltv2"
+}
+
+resource "netbox_prefix" "dev-bue1" {
+  prefix      = "10.1.11.0/24"
+  status      = "active"
+  site_id     = netbox_site.bue1.id
+  role_id     = netbox_ipam_role.dev.id
+  description = "R-DEV_NETGROUP-bue1"
+}
+
+resource "netbox_prefix" "dev-ltv2" {
+  prefix      = "10.2.11.0/24"
+  status      = "active"
+  site_id     = netbox_site.ltv2.id
+  role_id     = netbox_ipam_role.dev.id
+  description = "R-DEV_NETGROUP-ltv2"
+}
+
+resource "netbox_prefix" "sre-bue1" {
+  prefix      = "10.1.12.0/24"
+  status      = "active"
+  site_id     = netbox_site.bue1.id
+  role_id     = netbox_ipam_role.sre.id
+  description = "R-SRE_NETGROUP-bue1"
+}
+
+resource "netbox_prefix" "sre-ltv2" {
+  prefix      = "10.2.12.0/24"
+  status      = "active"
+  site_id     = netbox_site.ltv2.id
+  role_id     = netbox_ipam_role.sre.id
+  description = "R-SRE_NETGROUP-ltv2"
+}
+
+resource "netbox_prefix" "sec-bue1" {
+  prefix      = "10.1.13.0/24"
+  status      = "active"
+  site_id     = netbox_site.bue1.id
+  role_id     = netbox_ipam_role.sec.id
+  description = "R-SEC_NETGROUP-bue1"
+}
+
+resource "netbox_prefix" "sec-ltv2" {
+  prefix      = "10.2.13.0/24"
+  status      = "active"
+  site_id     = netbox_site.ltv2.id
+  role_id     = netbox_ipam_role.sec.id
+  description = "R-SEC_NETGROUP-ltv2"
 }
